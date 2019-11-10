15dc8477361d244d0a6f647751ee8e65470934ed
kuncevic <kncvch@gmail.com>
2019-11-10 23:22:24 +1100

    init

---
+++ projects/main-app/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+//import { SharedLibModule } from 'shared-lib/public-api';
+import { SharedLibModule } from 'projects/shared-lib/src/public-api';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedLibModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
